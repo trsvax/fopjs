85d0b676f010787b53c5cd12ff2e0279c48b8192
Barry Books <barry.books@dps.texas.gov>
2018-08-31 11:53:42 -0500

    add build scripts

--- test/index.js
+++ test/index.js
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDomServer from "react-dom/server";
 
-import fo from "../src/FO";
+import fo from "../index";
 
 const elementXML = ReactDomServer.renderToStaticMarkup(
   <fo.root>
     <fo.layoutMasterSet>
-      <fo.simplePageMaseter>
+      <fo.simplePageMaseter
+        master-name="content"
+        page-width="8.5in"
+        page-height="11in"
+        margin-top="1in"
+        margin-left="1.25in"
+        margin-right="1in"
+      >
         <fo.regionBody />
       </fo.simplePageMaseter>
     </fo.layoutMasterSet>
-    <fo.pageSequence>
-      <fo.flow />
+    <fo.pageSequence master-reference="content">
+      <fo.flow flow-name="xsl-region-body">
+        <fo.block>Hello World</fo.block>
+      </fo.flow>
     </fo.pageSequence>
   </fo.root>
 );
